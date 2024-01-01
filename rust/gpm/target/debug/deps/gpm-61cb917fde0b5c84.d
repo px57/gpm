c2ae157b57c95ac5d3b6f3efee03d27cc175a19f
projeta618 <projeta618@gmail.com>
2023-12-31 21:32:41 -0300

    Init the cargo project

---
+++ rust/gpm/target/debug/deps/gpm-61cb917fde0b5c84.d
@@ -0,0 +1,5 @@
+/home/david/Bureau/gpm2/rust/gpm/target/debug/deps/gpm-61cb917fde0b5c84: src/main.rs
+
+/home/david/Bureau/gpm2/rust/gpm/target/debug/deps/gpm-61cb917fde0b5c84.d: src/main.rs
+
+src/main.rs:
---
+++ rust/gpm/target/debug/gpm.d
@@ -0,0 +1,1 @@
+/home/david/Bureau/gpm2/rust/gpm/target/debug/gpm: /home/david/Bureau/gpm2/rust/gpm/src/main.rs
