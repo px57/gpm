ab2cf716e0f2a0546faffef7545ff6f52e53ad6c
projeta618 <projeta618@gmail.com>
2024-02-13 17:09:46 -0300

    feat: Integrate submodule repository and commit changes
    In order to integrate the submodule repository and commit changes, the following changes have been made:
    
    - Updated the commit message in `commandline__commit()` in `commit/commandline.py`
    - Added a new file `app/path/find.py` to find the root project gpm path.

--- app/commit/commandline.py
+++ app/commit/commandline.py
@@ -9,8 +9,14 @@ def commandline__commit():
     """
     This function is to commit the changes.
     """
-    print (Fore.RED + "Commit the changes" + Style.RESET_ALL)
+    print (Fore.RED + ">>> Integrate submodule repository..." + Style.RESET_ALL)
+    print (Fore.RED + ">>> Commit the changes..." + Style.RESET_ALL)
+    commit_command_line()
 
+
+def commit_command_line():
+    """
+    This function is to commit the changes.
+    """
     os.system('git add -A')
     os.system('commitgpt --suggestions 7 --max-tokens 100 ""')
-    # git add -A ; commitgpt --suggestions 7 --max-tokens 100 ""
---
+++ app/path/find.py
@@ -0,0 +1,12 @@
+"""
+
+"""
+
+
+from config.find import find_gpm_path
+
+def find_root_project_gpm_path():
+    """
+    This function is to find the root project gpm path
+    """
+    return find_gpm_path() + "/.gpm"
