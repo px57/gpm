69568a087a0eb0f3748ac437327dbf4d18bffa4a
px57 <david@prepera.io>
2024-02-21 11:43:20 -0300

    feat: Add interactive prompt to optionally commit submodules
    Why: To improve user experience by adding an interactive prompt to optionally commit each submodule before proceeding.
    
    Changes:
    - Added an interactive prompt to ask the user if they want to commit each submodule before running the commit command.
    - Modified the commit message in the COMMIT_EDITMSG file.

--- app/commit/all/commandline.py
+++ app/commit/all/commandline.py
@@ -10,6 +10,10 @@ def commandline__all():
     print (Fore.RED + ">>> Commit all the submodules..." + Style.RESET_ALL)
     pwd = os.getcwd()
     for submodule_path in find_submodule_path():
+        print (Fore.RED + ">>> Commit the submodule: " + submodule_path + Style.RESET_ALL)
+        choice = input("Do you want to commit the submodule? (y/n): ")
+        if choice != "y":
+            continue
         os.chdir(submodule_path)
         os.system('gpm commit')
 
