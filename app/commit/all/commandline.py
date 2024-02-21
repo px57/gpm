4a9423ba9b9ff91eeefd42d028e23370e2f52096
px57 <david@prepera.io>
2024-02-21 11:48:04 -0300

    feat: add function to check for project modification
    Why:
    - The codebase needed a function to check if the project has any modifications before committing.
    
    Changes:
    - Added a new function `has_modification(git_path: str)` to check for modifications in the project before committing.

--- app/commit/all/commandline.py
+++ app/commit/all/commandline.py
@@ -3,6 +3,23 @@ import os
 from colorama import Fore, Style
 from path.find import find_submodule_path
 
+def has_modification(git_path: str):
+    """
+    This function is to check if the project has modification.
+
+    Args:
+    - git_path: the git path.   
+    """
+    pwd = os.getcwd()
+    os.chdir(git_path)
+    a = os.system('git status')
+    os.chdir(pwd)
+    if a == 0:
+        return False
+    else:
+        return True
+
+
 def commandline__all():
     """
     This function is to commit all submodules.
@@ -10,6 +27,8 @@ def commandline__all():
     print (Fore.RED + ">>> Commit all the submodules..." + Style.RESET_ALL)
     pwd = os.getcwd()
     for submodule_path in find_submodule_path():
+        if not has_modification(submodule_path):
+            continue
         submodule_name = os.path.basename(submodule_path)
         print (Fore.RED + ">>> Commit the submodule: " + submodule_name.upper() + Style.RESET_ALL)
         choice = input("Do you want to commit the " + submodule_name.upper() + "? (y/n): ")
