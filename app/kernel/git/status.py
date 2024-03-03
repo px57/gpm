c6782a74ce959a623584d5b3f37d4ed7deda48dc
px57 <david@prepera.io>
2024-03-02 22:12:58 -0300

    chore: optimize check for modifications in git repository
    Why:
    - The existing code for checking modifications in the git repository needed optimization.
    
    Changes:
    - Updated the function to exclude certain directories ('.gitdisable/' and 'migrations/') from the check for modifications.
    - Printed a clearer output of the modifications found in the repository.
    - Added a check to return True if there are more than one modified files.

--- app/kernel/git/status.py
+++ app/kernel/git/status.py
@@ -10,19 +10,32 @@ def has_modification(git_path: str):
     - git_path: the git path.   
     """
     # Se déplacer dans le répertoire du dépôt Git
-    original_cwd = subprocess.os.getcwd()
+    # os.chdir(git_path)
 
     # Exécuter `git status --porcelain` pour voir s'il y a des modifications non commit
     result = subprocess.run(['git', 'status', '--porcelain'], stdout=subprocess.PIPE, text=True)
 
     # Revenir au répertoire original
-    subprocess.os.chdir(original_cwd)
+    subprocess.os.chdir(git_path)
 
     # Analyser le résultat
     if result.stdout:
-        print("Il y a des modifications non commit.")
-        print(result.stdout)
-        return True
+        # print("Il y a des modifications non commit.")
+        # print(result.stdout)
+        stdout = result.stdout
+        clear_stdout = []
+        for line in stdout.splitlines():
+            if '.gitdisable/' in line:
+                continue
+            if 'migrations/' in line:
+                continue
+            clear_stdout.append(line)
+        
+        print ('#########################################')
+        for line in clear_stdout:
+            print(line)
+        print ('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+        return len(clear_stdout) > 1
     else:
         print("Aucune modification non commit.")
         return False
