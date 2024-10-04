2c09befb632b296fb6e28b5efc3b17d89b6794f3
px57 <david@prepera.io>
2024-10-04 16:03:52 -0300

    y

--- setup.py
+++ setup.py
@@ -27,10 +27,7 @@ def create_command_file():
     """
     This function is used to create the command file.
     """
-    file = """
-#!/usr/bin/env python3
-
-
+    file = """#!/usr/bin/env python3
 import os
 import sys
 
@@ -38,20 +35,66 @@ argv = sys.argv[1:]
 command = 'python3 /var/projects/gpm/app/main.py ' + ' '.join(argv)
 os.system(command)
 """
+    os.system('sudo touch /usr/local/bin/gpm')
+    os.system('sudo chmod 777 /usr/local/bin/gpm')
+
     with open('/usr/local/bin/gpm', 'w') as f:
         f.write(file)
 
+    # Delete the first "\n" in the /user/local/bin/gpm
+
     os.system(
         'chmod +x /usr/local/bin/gpm'
+    )
+
+def save_chatgpt_api_key():
+    """
+
+    """
+    key = input("Enter your OpenAI ChatGPT API key: ")
+
+    os.system(
+        'mkdir -p ~/.config/commitgpt'
+    )
+    # mkdir -p ~/.config/commitgpt
+
+    os.system(
+        'touch ~/.config/commitgpt/config.toml'
+    )
+
+    # touch ~/.config/commitgpt/config.toml
+
+    os.system(
+        f'echo api_key = "{key}" > ~/.config/commitgpt/config.toml'
+    )
+    # echo 'api_key = "YOUR_OPENAI_API_KEY"' > ~/.config/commitgpt/config.toml
+
+def setup_requirements():
+    """ 
+    This function is used to install the requirements.
+    """
+    # Python requirements
+    os.system(
+        'pip3 install -r requirements.text'
+    )
+
+    # Cargo requirements
+    os.system(
+        'export PATH="$HOME/.cargo/bin:$PATH"'
+    )
+    os.system(
+        'sudo cargo install commitgpt'
     )
 
 def main():
     """
     This function is used to install the package using pip.
     """
+    setup_requirements()
     create_directories()
     moove_directories()
     create_command_file()
+    save_chatgpt_api_key()
 
 if __name__ == "__main__":
     main()
